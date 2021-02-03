ee625c91f62c5023f1a3937edc2a7cdd4e76bad7
Atri Hegde <iamatrihegde@gmail.com>
2021-02-03 18:02:16 +0000

    Fixed user account not saving

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
         for (String x : animatedWelcome) {
             System.out.print(x);
             try {
-                Thread.sleep(500);
+                Thread.sleep(1500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
